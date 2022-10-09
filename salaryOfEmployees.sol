afa5adb0e065ebc08de538a2f924b817bc473c42
Yusuf Ozdemir Arslan <yusufozdemirarslan@gmail.com>
2022-10-09 13:12:05 +0300

    com

---
+++ salaryOfEmployees.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+contract salaryOfEmployees {
+    // employer address defined
+    address employer;
+
+    event salary_transferred(address addr, uint amount, uint contract_balance);
+
+    constructor() {
+        employer = msg.sender;
+    }
+
+    // define employee struct
+    struct Employee {
+        address payable wallet_address;
+        string first_name;
+        string last_name;
+        uint release_time;
+        uint amount;
+        bool can_withdraw;
+    }
